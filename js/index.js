a899e8ca51e2e16d85dedfe7915bec3dba5ad4a3
ARCX7 <arcx7x@gmail.com>
2025-06-09 14:44:03 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const title = document.querySelector('.title')
-const text = "Dek dinda sayang";
-console.log(text.slice(0, 3)); // "Dek"
+const text = 'I Have Something'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
